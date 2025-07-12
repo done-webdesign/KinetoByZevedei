7eae7b1f81b44bd9db7659585fa21b8018d84f3d
Robu David <done_web@yahoo.com>
2025-07-12 23:06:35 +0300

    changed videos

--- src/components/videos.tsx
+++ src/components/videos.tsx
@@ -1,11 +1,12 @@
 import { useState, useEffect } from "react";
 import ReactPlayer from "react-player";
-import video1 from "../assets/videos/video-1.mp4";
-import video2 from "../assets/videos/video-2.mp4";
-import video3 from "../assets/videos/video-3.mp4";
-import video4 from "../assets/videos/video-4.mp4";
 
-const videos = [video1, video2, video3, video4];
+const videos = [
+  "public/videos/video-1.MP4",
+  "public/videos/video-2.MP4",
+  "public/videos/video-3.MP4",
+  "public/videos/video-4.MP4",
+];
 
 export default function VideoCarousel() {
   const [currentStart, setCurrentStart] = useState(0);
